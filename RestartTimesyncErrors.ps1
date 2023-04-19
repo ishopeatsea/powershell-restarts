bb72131fe312a1405b0a0617c5073bb15c1cc557
ishopeatsea <71855477+ishopeatsea@users.noreply.github.com>
2023-04-19 10:57:13 +1000

    basic timesync restarting
    added script for pulling information from labslogger website as a list of PC objects
    
    added script that batch restarts all PC objects where the timesync reports an error

---
+++ RestartTimesyncErrors.ps1
@@ -0,0 +1,29 @@
+﻿Write-Host "Retrieving data..."
+$timesyncErrors = & $PSScriptRoot\getPcObjectsFromLabslogger.ps1 | where {($_.TimeSync -like "*error*") -and ($_.'Last Ping' -like "*Up")}
+$total = $timesyncErrors.Count
+Write-Host "Found $total timesync errors."
+$count = 0
+
+foreach ($Pc in $timesyncErrors)
+{
+    if (($Pc.'IP Address' -like "Expected:*") -or ($Pc.'IP Address' -like "*not match*")) { $Pc.'IP Address' = @($Pc.'IP Address' -split ':')[2].Trim() }
+
+    # Attempt restart
+    Restart-Computer -ComputerName $Pc.'IP Address' -ErrorVariable errmsg -ErrorAction SilentlyContinue
+
+    if ($errmsg -ne $null) {
+        # Error log: Restart failed
+        if ($errmsg -like "*other users logged on*")
+        {
+            Write-Host "$($Pc.'Computer Name') failed: $(@($errmsg -split ':')[1].Trim())" -ForegroundColor yellow
+        } else {
+            Write-Host "$($Pc.'Computer Name') failed: $(@($errmsg -split ':')[1].Trim())" -ForegroundColor red
+        }
+    }
+    else {
+        # Success
+        Write-Host "$($Pc.'Computer Name') restarted." -ForegroundColor green
+        $count++
+    }
+}
+Write-Host "Restarted $count/$total computers."
